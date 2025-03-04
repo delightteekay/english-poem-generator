f1ab0d7347f352020d73c7e39dd51607a5edf427
delightteekay <162933257+delightteekay@users.noreply.github.com>
2025-03-04 02:42:09 +0200

    added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
-function generatePoem(event) {
-event.preventDefault();
-
+function displayPoem(response) {
 new Typewriter('#poem', {
-strings: "Nature's first green is gold",
+strings: response.data.answer,
 autoStart: true,
 delay: 1,
 cursor: "",
 });
 
-
-
 }
 
 
+function generatePoem(event) {
+event.preventDefault();
+
+let instructionsInput = document.querySelector("#user-instructions");
+let apiKey = "td08be15cf102abbf4da48f50db4e34o";
+let prompt = `Generate a english poem about ${instructionsInput.value}`;
+let context = "";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+axios.get(apiUrl).then(displayPoem);
+
+}
 
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
